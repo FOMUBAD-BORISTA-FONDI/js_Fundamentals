533446f83131dc61d3532e60c6491fe2792aad4e
FOMUBAD-BORISTA-FONDI <borista.fomubad@facsciences-uy1.cm>
2026-04-14 13:04:16 +0100

    .

--- async_lab.js
+++ async_lab.js
@@ -65,7 +65,7 @@ const getPostInfo = async (PostId) => {
     console.log(`Post Body: ${data.body}`);
   } catch (error) {
     console.log("Error: ", error.message);
-    console.error("Error whilefetching post data:", error);
+    console.error("Error while fetching post data:", error);
     console.log("Please check the API endpoint and try again.");
   }
 };
